22a1c91808a034a19d1137d65cb67d065186680d
dwshea <dwshea@mavs.coloradomesa.edu>
2025-09-19 15:37:53 +0000

    notes.py

--- notebooks/notes.py
+++ notebooks/notes.py
@@ -1,6 +1,12 @@
 def multiply(var1: int, var2: int):
-    answer = var1 * var2
+    answer: int = var1 * var2
     return answer
 
 print(multiply(8,6))
 
+def add(a: float, b: float) -> float:
+    answer: float = a + b
+    return answer
+
+assert add(2, 3) == 5
+assert add(10, -5) == 5
