20ebfa9e876c10742454e27b8cfc831291e4df5d
dwshea <dwshea@mavs.coloradomesa.edu>
2025-09-26 15:01:42 +0000

    notes

--- notebooks/notes.py
+++ notebooks/notes.py
@@ -1,3 +1,4 @@
+"""
 def multiply(var1: int, var2: int) -> int:
     answer: int = var1 * var2
     return answer
@@ -14,3 +15,11 @@ assert add(100, 99.9) == 199.9
 #assert add(99.99, 1.99) == 101.98  --- false becasue the computer thinks the answer is 101.97999999999
 assert abs(add(99.99, 1.99) - 101.98) < 0.000001
 print('All test cases passed!')
+"""
+
+line = input()
+a, b = line.split()
+a = int(a)
+b = int(b)
+
+print(f"I have {a} apples, and {b} bannanas")
